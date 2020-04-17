20bb39c0c1e025d10c99d017ab3b27a822efc4b9
@rachael.colby <rachael.colby@dharbor.com>
2020-04-17 13:54:43 -0700

    added csm-module

---
+++ projects/csm-poc-lib/src/lib/csm-module/csm-module.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-csm-module',
+  templateUrl: './csm-module.component.html',
+  styleUrls: ['./csm-module.component.css']
+})
+export class CsmModuleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- projects/csm-poc-lib/src/lib/csm-poc-lib.module.ts
+++ projects/csm-poc-lib/src/lib/csm-poc-lib.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CsmPocLibComponent } from './csm-poc-lib.component';
+import { CsmModuleComponent } from './csm-module/csm-module.component';
 
 @NgModule({
   imports: [
   ],
-  declarations: [CsmPocLibComponent],
+  declarations: [CsmPocLibComponent, CsmModuleComponent],
   exports: [CsmPocLibComponent]
 })
 export class CsmPocLibModule { }
