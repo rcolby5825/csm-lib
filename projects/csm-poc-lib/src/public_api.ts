6998507e35ed625817610ea430f6db7414763f3f
@rachael.colby <rachael.colby@dharbor.com>
2020-04-29 09:50:33 -0700

    changed public api settings

--- projects/csm-poc-lib/src/public_api.ts
+++ projects/csm-poc-lib/src/public_api.ts
@@ -5,3 +5,6 @@
 export * from './lib/csm-poc-lib.service';
 export * from './lib/csm-poc-lib.component';
 export * from './lib/csm-poc-lib.module';
+export * from './lib/csm-module/engine/process-command.service';
+export * from './lib/csm-module/services/lucy-q-and-a.service';
+export * from './lib/csm-module/services/mssql-connect.service';
