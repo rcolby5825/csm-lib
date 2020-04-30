ee87133f1ce4314fe5eb490f768b1edff13d9def
@rachael.colby <rachael.colby@dharbor.com>
2020-04-30 08:47:45 -0700

    defining public api endpoints for lib

--- projects/csm-poc-lib/src/public_api.ts
+++ projects/csm-poc-lib/src/public_api.ts
@@ -2,9 +2,15 @@
  * Public API Surface of csm-poc-lib
  */
 
-export * from './lib/csm-poc-lib.service';
-export * from './lib/csm-poc-lib.component';
-export * from './lib/csm-poc-lib.module';
 export * from './lib/csm-module/engine/process-command.service';
 export * from './lib/csm-module/services/lucy-q-and-a.service';
+export * from './lib/csm-module/conversations/conversation.service';
+
+
+// ones that are being put in - but probably won't be kept in final
+export * from './lib/csm-module/command-pattern/context-invoker';
 export * from './lib/csm-module/services/mssql-connect.service';
+
+// doesn't work will need to see about a different way to import the component
+// export * from './lib/csm-module/avatar-papertape-modal/avatar-papertape-modal.component';
+
